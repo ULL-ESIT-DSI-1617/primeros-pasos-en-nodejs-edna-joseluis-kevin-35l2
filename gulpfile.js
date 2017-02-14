244e166a71f8e606221063780aa98edbdc847957
opelu <opelu8@gmail.com>
2017-02-14 19:24:53 +0000

    Modificando gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,14 @@
-/*
-* Dependencias
-*/
+
+// Dependencias
+
 
 var gulp = require('gulp');
 var shell = require('gulp-shell');
+var ghPages = require('gulp-gh-pages');
 
-/*
- Tarea Build -------------------------------------------------------
-*/
+
+// Tarea Build -------------------------------------------------------
+
 
 gulp.task('build', function() {
   return gulp.src('').pipe(shell(['./scripts/generate-gitbook']));
@@ -16,16 +17,20 @@ gulp.task('build', function() {
 
 /*
  Tarea Deploy ------------------------------------------------------
-*/
 
 gulp.task('deploy', function () {
   return gulp.src('').pipe(shell(["./scripts/deploy-gitbook"]));
 });
-
-
-/*
- Tarea Push al Repositorio -----------------------------------------
 */
+
+
+gulp.task('deploy', function() {
+      return gulp.src('./minified/**/*')
+        .pipe(ghPages());
+    });
+
+
+// Tarea Push al Repositorio -----------------------------------------
 
 gulp.task('push-repo', function(){
     return gulp.src('').pipe(shell([
@@ -36,8 +41,8 @@ gulp.task('push-repo', function(){
 });
 
 
-/*
- Tarea por defecto -------------------------------------------------
-*/
+
+// Tarea por defecto -------------------------------------------------
+
 
 gulp.task('default', ['build', 'deploy']);
