7f06836441eae6047a610fd6cdb41b1c2a584a22
Edna Galiano <ednagc.11@gmail.com>
2017-02-17 17:55:38 +0000

    Actualizando server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 
 // instanciar
 var app = express()
-// var path = require('path')
+var path = require('path')
 app.set('port', (process.env.PORT || 8080));
 
 
@@ -20,7 +20,10 @@ app.get('/about', function(req, res){
 });
 */
 
-app.use(express.static('./gh-pages'))
+
+app.use(express.static(path.join(__dirname,'gh-pages')));
+
+// app.use(express.static('./gh-pages'))
 
 app.get('/', function (req, res) {
   res.send('index');
