4def996112a72409cb106dfee27689cbb2d60b5b
alu0100821390 <alu0100821390@ull.edu.es>
2017-02-18 17:58:27 +0000

    Actualizar server.js

--- server.js
+++ server.js
@@ -1,36 +1,21 @@
-// importar
+// Importar
 var express = require('express');
 
 // instanciar
-var app = express()
-var path = require('path')
+var app = express();
+var path = require('path');
 app.set('port', (process.env.PORT || 8080));
 
 
-
-// ruteo
-app.get('/', function(req, res){
-  res.sendfile(__dirname + '/gh-pages/index.html');
-});
-
-
-/* 
-app.get('/about', function(req, res){
-  res.sendfile(__dirname + '/public/about.html');
-});
-*/
-
-
-app.use(express.static(path.join(__dirname,'gh-pages')));
-
-// app.use(express.static('./gh-pages'))
+// Ruteo
+app.use(express.static(path.join('/gh-pages')));
 
 app.get('/', function (req, res) {
-  res.send('index');
-})
+    res.sendFile('index');
+});
 
 
-// escuchar
-app.listen(app.get('port'), function() {
-  console.log('Servidor corriendo... Acceda a 10.6.129.247:', app.get('port'));
+// Escuchar
+var server = app.listen(app.get('port'), function () {
+    console.log('Servidor escuchando en el puerto %s', app.get('port'));
 });
